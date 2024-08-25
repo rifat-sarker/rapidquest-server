256be08f817771c61b6da254c84d6e793ce3ff18
Rifat Sarker <rifatswd@gmail.com>
2024-08-25 21:40:41 +0600

    connect to mongodb and get data

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aaflc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+app.use(cors());
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n10ox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri);
 
 async function run() {
   try {
-    await client.connect()
+    await client.connect();
     console.log("Connected to MongoDB server");
-    const database = client.db("RQ_Analytics database").collection('shopifyCustomers');
 
+    const shopifyCustomersCollection = client
+      .db("RQ_Analytics")
+      .collection("shopifyCustomers");
 
-    app.get('/customers', async(req,res)=> {
-        const cursor = database.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+    const shopifyProductsCollection = client
+      .db("RQ_Analytics")
+      .collection("shopifyProducts");
 
+    const shopifyOrdersCollection = client
+      .db("RQ_Analytics")
+      .collection("shopifyOrders");
+
+    // customers
+    app.get("/customers", async (req, res) => {
+      const cursor = shopifyCustomersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // products
+    app.get("/products", async (req, res) => {
+      const cursor = shopifyProductsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // orders
+    app.get("/orders", async (req, res) => {
+      const cursor = shopifyOrdersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
